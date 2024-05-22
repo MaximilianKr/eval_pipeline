4256902453a18c1609e51331e89757c3d118255f
przvlprd <przvlprd@pm.me>
2024-05-22 16:05:01 +0200

    add dataset as input argument

--- run_eval.sh
+++ run_eval.sh
@@ -3,15 +3,15 @@
 # Template
 # bash run_eval.sh {dataset} {model} {optional: revision} {optional: quantization}
 
-# TODO: Add dataset?
-DATASET="dtfit"
+# Required, e.q., "dtfit"
+DATASET=$1
 
 # Required, e.g., "EleutherAI/pythia-14m", "allenai/OLMo-1B-hf"
-MODEL=$1
+MODEL=$2
 
 # Optional, default "main" / see intermediate checkpoints for models
 # TODO: Validate revision input?
-REVISION=${2:-"main"}
+REVISION=${3:-"main"}
 
 # Extract the second part of the model name
 if [[ $MODEL == */* ]]; then
@@ -23,6 +23,15 @@ fi
 
 # Determine the directory where the script is located
 SCRIPT_DIR=$(dirname "$0")
+
+# Construct path to the dataset directory relative to the script's location
+DATASET_DIR="$SCRIPT_DIR/data/$DATASET"
+
+# Check if the dataset directory exists
+if [ ! -d "$DATASET_DIR" ]; then
+    echo "Error: Dataset directory '$DATASET_DIR' does not exist. Either check your input or add a new dataset."
+    exit 1
+fi
 
 # Construct path relative to the script's location
 PYTHON_SCRIPT="$SCRIPT_DIR/bin/run_experiment.py"
