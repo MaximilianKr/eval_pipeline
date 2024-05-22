638ee51b5d99f6f1915ad7367190e562d1ba423c
przvlprd <przvlprd@pm.me>
2024-05-22 15:27:03 +0200

    add wip backend code

---
+++ bin/io.py
@@ -0,0 +1,38 @@
+import json
+from time import gmtime, strftime
+from minicons import scorer
+import torch
+
+torch.set_grad_enabled(False)
+
+
+def timestamp():
+    return strftime("%Y-%m-%d %H:%M:%S", gmtime())
+
+def dict2json(d: dict, out_file: str):
+    with open(out_file, "w") as fp:
+        json.dump(d, fp, indent=2)
+
+def initialize_model(model_name: str, revision: str, quantization: str):
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+        print("Set device to CUDA")
+    else:
+        device = torch.device("cpu")
+        print("Using CPU (CUDA unvailable); adjust your expectations") 
+    # TODO: add backends: 'togethercomputer/RedPajama-INCITE-7B-Base'
+    #                     'TinyLlama/TinyLlama-1.1B'
+    #                     'Zyphra/Zamba-7b'
+    if "pythia" or "allenai" in model_name:
+        # TODO: fix quantization with minicons?
+        model = scorer.IncrementalLMScorer(
+            model=model_name, 
+            device=device, 
+            revision=revision
+            )
+    else:
+        raise ValueError(
+            f"Model not (yet) supported! (Your model: {model_name})"
+        )
+    
+    return model
