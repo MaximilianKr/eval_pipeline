3d45d0f6b89d14b996934936099e72baaf5c5a92
przvlprd <przvlprd@pm.me>
2024-06-26 17:19:16 +0200

    refactor: change reduction method to normalize by token number, add type hints, update docstrings

--- bin/run_experiment.py
+++ bin/run_experiment.py
@@ -5,52 +5,72 @@ from bin.io import initialize_model, timestamp, dict2json
 from minicons import scorer
 
 
-def run_experiment(model: scorer.IncrementalLMScorer, meta_data: dict, file_out: str):
-    # Read corpus data
+def run_experiment(
+    model: scorer.IncrementalLMScorer, meta_data: dict, file_out: str
+) -> None:
+    """
+    Run the experiment for the given model and dataset and save the results to
+    a JSON file with metadata.
+
+    Args:
+        model (scorer.IncrementalLMScorer): The model to evaluate.
+        meta_data (dict): Metadata about the model and dataset.
+        file_out (str): The path to the output file.
+
+    Returns:
+        None
+    """
     dataset = meta_data["dataset"]
     df = pd.read_csv(f"./data/{dataset}/corpus.csv")
 
     results = []
 
     print("Running experiment...")
-    # TODO: Run batched (all instances passed at once to minicons)
-    # Run row-by-row with tqdm
+
     for _, row in tqdm(list(df.iterrows()), total=len(df.index)):
         good_instance = f"{row.prefix} {row.good_continuation}"
         bad_instance = f"{row.prefix} {row.bad_continuation}"
         stimuli = [good_instance, bad_instance]
 
-        # Sequence Log-probability
-        # reduction = lambda x: x.sum(0).item()
+        # Sequence Log-probability, normalized by number of tokens
+        # reduction = lambda x: x.mean(0).item()
         # see https://github.com/kanishkamisra/minicons
-        logprobs = model.sequence_score(stimuli, reduction = lambda x: x.sum(0).item())
-        
-        # Store results in dictionary
+        logprobs = model.sequence_score(stimuli, reduction=lambda x: x.mean(0).item())
+
         res = {
             "item_id": row.item_id,
             "prefix": row.prefix,
             "good_continuation": row.good_continuation,
             "bad_continuation": row.bad_continuation,
             "logprob_of_good_continuation": logprobs[0],
-            "logprob_of_bad_continuation": logprobs[1]
+            "logprob_of_bad_continuation": logprobs[1],
         }
-        
-        # Record results for current item
         results.append(res)
 
-    # Combine meta information with model results into one dict
-    output = {
-        "meta": meta_data,
-        "results": results
-    }
+    output = {"meta": meta_data, "results": results}
 
-    # Export results
     dict2json(output, file_out)
 
 
-def main():
+def main() -> None:
+    """
+    Main function to run the evaluation. It should be called from 'run_eval.py'
+    in the parent directory with following command line arguments:
+        - model_name: The name of the model to evaluate.
+        - revision: The revision of the model to evaluate.
+        - dataset: The dataset to evaluate the model on.
+        - file_out: The path to the output file.
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
     if len(sys.argv) < 4:
-        print("Usage: Incorrect call. Check the documentation on how to run the evaluation.") 
+        print(
+            "Usage: Incorrect call. Check the documentation on how to run the evaluation."
+        )
         sys.exit(1)
 
     model_name, revision, dataset, file_out = sys.argv[1:5]
@@ -62,11 +82,11 @@ def main():
         "model": model_name,
         "revision": revision,
         "dataset": dataset,
-        "timestamp": timestamp()
+        "timestamp": timestamp(),
     }
 
     run_experiment(model, meta_data, file_out)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
